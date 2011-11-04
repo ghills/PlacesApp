68cc35d4db39891714524ee3c7a6e5f1383051c8
Gavin Hills <ghills@gmail.com>
2011-11-03 21:50:19 -0700

    Adding FlickrFetcher files provided for the assignment.

---
+++ Places/FlickrAPIKey.h
@@ -0,0 +1,10 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2010
+//  Copyright Stanford University
+//
+
+// Put your Flickr API key here or nothing will work.
+
+#define FlickrAPIKey @""
