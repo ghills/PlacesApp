6c9a82eaa0646dceaead6c935725727fbd96bab5
Gavin Hills <ghills@gmail.com>
2011-11-03 21:45:00 -0700

    Initial Commit

---
+++ Places/PlacesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PlacesAppDelegate.h
+//  Places
+//
+//  Created by Gavin Hills on 11/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
